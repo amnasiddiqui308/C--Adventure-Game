825495bfcfd6e5a7e780ae60a5241012e1e22fa8
unknown <amnasiddiqui308@gmail.com>
2024-05-29 13:25:41 +0500

    Variables Initialized

--- Main.cpp
+++ Main.cpp
@@ -1,8 +1,11 @@
 #include<iostream>
+#include<string>
 using namespace std;
 
 int main() {
-	cout << "Hello Daniyal" << " BIEEEE" << endl;
-	cout << "MEOW" << endl;
+	int playerHP = 15, monsterHP = 20, riversideCount = 3;
+	bool hasKilledMonster = false;
+	string weapons[5][5] = { {"Knife", "Taken"}, {"Sword", "Not Taken"}, {"Mace", "Not Taken"}, {"Axe", "Not Taken"}, {"Arrow", "Not Taken"} };
+	
 	return 0;
 }
