6fbb273e6f04918325de621a3a778ef1e339494b
unknown <amnasiddiqui308@gmail.com>
2024-05-30 01:53:14 +0500

    West Function Implemented and Level1 Completed

--- Main.cpp
+++ Main.cpp
@@ -1,10 +1,131 @@
 #include<iostream>
 #include<string>
+#include<cstdlib>
+#include<time.h>
+
 using namespace std;
 
-void crossRoad(bool& hasKilledMonster, int& playerHP, int& riversideCount, string weapons[][5]);
+void crossRoad(bool& hasKilledMonster, int& playerHP, int& riversideCount, string weapons[][5], int& monsterHP);
 
-void east(bool& hasKilledMonster, int& playerHP, int& riversideCount, string weapons[][5]) {
+void fightingMonster(int& playerHP, int& monsterHP, char& giveDamage, int& weapon, int& previousHP, double& monsterDamageFactor) {
+	if (playerHP <= 0 || monsterHP <= 0)
+		return;
+
+	srand(time(0));
+	cout << "You are now fighting the monster" << endl;
+	cout << "The monster's HP is: " << monsterHP << "\nYour HP is: " << playerHP << endl;
+	
+	//input validation
+	do {
+		cout << "Press x to give damage to the monster" << endl;
+		cout << "'PRESS 0 TO EXIT'" << endl;
+		cin >> giveDamage;
+
+		if (giveDamage == '0') 
+			exit(0);
+	
+		if (giveDamage != 'x') 
+			cout << "INVALID KEY PRESSED!" << endl;
+		
+	} while (giveDamage != 'x');
+
+	if (weapon == 1) {
+		monsterHP = monsterHP - (rand() % 5 + 1);
+
+		if (monsterHP > 0) {
+			//reducing monster's power after a damage of 5
+			if (monsterHP <= (previousHP - 5)) {
+				monsterDamageFactor += 0.5;
+				previousHP = monsterHP;
+			}
+
+			playerHP = (playerHP - (rand() % 9 + 1)) + monsterDamageFactor;
+		}
+	}
+	else if (weapon == 2) {
+		monsterHP = monsterHP - (rand() % 6 + 5); //giving monster more damage because of the sword
+
+		if (monsterHP > 0) {
+			//reducing monster's power after a damage of 5
+			if (monsterHP <= (previousHP - 5)) {
+				monsterDamageFactor += 0.5;
+				previousHP = monsterHP;
+			}
+			
+			playerHP = (playerHP - (rand() % 5 + 1)) + monsterDamageFactor; //monster giving lesser damage because player is using the sword
+		}
+	}
+	
+	fightingMonster(playerHP, monsterHP, giveDamage, weapon, previousHP, monsterDamageFactor);
+}
+
+void west(bool& hasKilledMonster, int& playerHP, int& riversideCount, string weapons[][5], int& monsterHP) {
+	int choice = -1, weapon = 1, previousHP = monsterHP;
+	double monsterDamageFactor = 0.0;
+	char giveDamage = 0, option = 0;
+
+	if (hasKilledMonster) {
+		cout << "You have already killed the Goblin" << endl;
+		cout << "Press any key to continue" << endl;
+		cout << "'PRESS 0 TO EXIT'" << endl;
+
+		cin >> option;
+		if (option == '0')
+			exit(0);
+		else
+			crossRoad(hasKilledMonster, playerHP, riversideCount, weapons, monsterHP);
+	}
+
+	cout << "You encounter a Goblin!" << endl;
+	cout << "1. Fight\n2. Run" << endl;
+	cout << "'PRESS 0 TO EXIT'" << endl;
+
+	cin >> choice;
+
+	if (choice == 0)
+		exit(0);
+
+	if(choice == 1) {
+		if (weapons[1][1] == "Taken") {
+			cout << "Choose your Weapon before the Fight." << endl;
+			cout << "1. Knife\n2. Sword" << endl;
+			cin >> weapon;
+		}
+
+		fightingMonster(playerHP, monsterHP, giveDamage, weapon, previousHP, monsterDamageFactor);
+
+		if (playerHP <= 0 && monsterHP <= 0) {
+			hasKilledMonster = true;
+			cout << "You Killed The Goblin!" << endl;
+			cout << "YOU DIED!" << endl;
+			exit(0);
+		}
+
+		else if (playerHP <= 0) {
+			cout << "YOU DIED" << endl;
+			exit(0);
+		}
+
+		else {
+			cout << "You Killed The Goblin!\nYou obtained the monster's silver ring." << endl;
+			hasKilledMonster = true;
+			playerHP += 5;
+
+			cout << "Your HP is now: " << playerHP << endl;
+			cout << "------------------------------------------------------------------------" << endl;
+			cout << "Press any key to continue" << endl;
+			cout << "'PRESS 0 TO EXIT'" << endl;
+			cin >> option;
+
+			if (option == '0')
+				exit(0);
+		}
+	}
+
+	crossRoad(hasKilledMonster, playerHP, riversideCount, weapons, monsterHP);
+}
+
+void east(bool& hasKilledMonster, int& playerHP, int& riversideCount, string weapons[][5], int& monsterHP) {
 	char choice = 0;
 
 	if (weapons[1][1] == "Taken") {
@@ -23,10 +144,10 @@ void east(bool& hasKilledMonster, int& playerHP, int& riversideCount, string wea
 	if (choice == '0')
 		exit(0);
 	else
-		crossRoad(hasKilledMonster, playerHP, riversideCount, weapons);
+		crossRoad(hasKilledMonster, playerHP, riversideCount, weapons, monsterHP);
 }
 
-void north(bool& hasKilledMonster, int& playerHP, int& riversideCount, string weapons[][5]) {
+void north(bool& hasKilledMonster, int& playerHP, int& riversideCount, string weapons[][5], int& monsterHP) {
 	char choice = 0;
 
 	if (riversideCount <= 0) {
@@ -51,10 +172,10 @@ void north(bool& hasKilledMonster, int& playerHP, int& riversideCount, string we
 	if (choice == '0')
 		exit(0);
 	else
-		crossRoad(hasKilledMonster, playerHP, riversideCount, weapons);
+		crossRoad(hasKilledMonster, playerHP, riversideCount, weapons, monsterHP);
 }
 
-void south(bool& hasKilledMonster, int& playerHP, int& riversideCount, string weapons[][5]) {
+void south(bool& hasKilledMonster, int& playerHP, int& riversideCount, string weapons[][5], int& monsterHP) {
 	int choice = 0;
 
 	do {
@@ -99,7 +220,7 @@ void south(bool& hasKilledMonster, int& playerHP, int& riversideCount, string we
 			break;
 
 		case 3:
-			crossRoad(hasKilledMonster, playerHP, riversideCount, weapons);
+			crossRoad(hasKilledMonster, playerHP, riversideCount, weapons, monsterHP);
 			break;
 
 		default:
@@ -111,7 +232,7 @@ void south(bool& hasKilledMonster, int& playerHP, int& riversideCount, string we
 		exit(0);
 }
 
-void crossRoad(bool& hasKilledMonster, int& playerHP, int& riversideCount, string weapons[][5]) {
+void crossRoad(bool& hasKilledMonster, int& playerHP, int& riversideCount, string weapons[][5], int& monsterHP) {
 	int choice = 0;
 
 	cout << "You are at the crossroad. If you go South, you'll be at the town's gate." << endl;
@@ -132,21 +253,22 @@ void crossRoad(bool& hasKilledMonster, int& playerHP, int& riversideCount, strin
 
 	case 1:
 		cout << "North" << endl;
-		north(hasKilledMonster, playerHP, riversideCount, weapons);
+		north(hasKilledMonster, playerHP, riversideCount, weapons, monsterHP);
 		break;
 
 	case 2:
 		cout << "East" << endl;
-		east(hasKilledMonster, playerHP, riversideCount, weapons);
+		east(hasKilledMonster, playerHP, riversideCount, weapons, monsterHP);
 		break;
 
 	case 3:
 		cout << "South" << endl;
-		south(hasKilledMonster, playerHP, riversideCount, weapons);
+		south(hasKilledMonster, playerHP, riversideCount, weapons, monsterHP);
 		break;
 
 	case 4:
 		cout << "West" << endl;
+		west(hasKilledMonster, playerHP, riversideCount, weapons, monsterHP);
 		break;
 
 	default:
@@ -162,6 +284,6 @@ int main() {
 	bool hasKilledMonster = false;
 	string weapons[5][5] = { {"Knife", "Taken"}, {"Sword", "Not Taken"}, {"Mace", "Not Taken"}, {"Axe", "Not Taken"}, {"Arrow", "Not Taken"} };
 
-	crossRoad(hasKilledMonster, playerHP, riversideCount, weapons);
+	crossRoad(hasKilledMonster, playerHP, riversideCount, weapons, monsterHP);
 	return 0;
 }
